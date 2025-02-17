50a8cc97d42bdd9bc895f8b149d3764dcc618db1
Jie Yang <jie.yang.uts@gmail.com>
2025-02-17 14:55:48 +1100

    Update mindisttwinsloc.m

--- mindisttwinsloc.m
+++ mindisttwinsloc.m
@@ -1,4 +1,17 @@
 function [linkloc1,linkloc2] = mindisttwinsloc(dataloc1,dataloc2,DM)
+% -----------------------------------------------------------------------------
+%  Torque Clustering - Matlab Implementation
+%  Copyright (C) Jie Yang
+%
+%  Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+%  International (CC BY-NC-SA 4.0)
+%
+%  This code is intended for academic and research purposes only.
+%  Commercial use is strictly prohibited. Please contact the author for licensing inquiries.
+%
+%  Author: Jie Yang (jie.yang.uts@gmail.com)
+% -----------------------------------------------------------------------------
+
 %MINDISTLOC Summary of this function goes here
 %   Detailed explanation goes here
 %num1=size(dataloc1,2);
