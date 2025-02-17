d01425833d825725d8fd6e316e5b2c346dba45b6
Jie Yang <jie.yang.uts@gmail.com>
2025-02-17 14:48:59 +1100

    Update TorqueClustering_Run.m

--- TorqueClustering_Run.m
+++ TorqueClustering_Run.m
@@ -1,3 +1,18 @@
+% -----------------------------------------------------------------------------
+%  Torque Clustering - Matlab Implementation
+%  Copyright (C) Jie Yang
+%
+%  Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+%  International (CC BY-NC-SA 4.0)
+%
+%  This code is intended for academic and research purposes only.
+%  Commercial use is strictly prohibited. Please contact the author for licensing inquiries.
+%
+%  Author: Jie Yang (jie.yang.uts@gmail.com)
+% -----------------------------------------------------------------------------
+
+
+
 addpath(genpath('../data'))
 
 
