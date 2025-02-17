8950424e4c44808cc3fe4141e7a4a5d0385fc3fd
Jie Yang <jie.yang.uts@gmail.com>
2025-02-17 14:58:02 +1100

    Update uniqueZ.m

--- uniqueZ.m
+++ uniqueZ.m
@@ -1,4 +1,17 @@
 function [newZ,ljmat] = uniqueZ(Z,old_ljmat)
+% -----------------------------------------------------------------------------
+%  Torque Clustering - Matlab Implementation
+%  Copyright (C) Jie Yang
+%
+%  Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+%  International (CC BY-NC-SA 4.0)
+%
+%  This code is intended for academic and research purposes only.
+%  Commercial use is strictly prohibited. Please contact the author for licensing inquiries.
+%
+%  Author: Jie Yang (jie.yang.uts@gmail.com)
+% -----------------------------------------------------------------------------
+
 %UNIQUEZ Summary of this function goes here
 %   Detailed explanation goes here
 if isempty(Z)==1
