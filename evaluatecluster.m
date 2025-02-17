11e16c456eb057a38ae63284e5694268fc0e8d84
Jie Yang <jie.yang.uts@gmail.com>
2025-02-17 14:54:20 +1100

    Update evaluatecluster.m

--- evaluatecluster.m
+++ evaluatecluster.m
@@ -1,6 +1,19 @@
 function [NMI,ACC] = evaluatecluster(Idx,datalabels)
-%EVALUATECLUSTER 此处显示有关此函数的摘要
-%   此处显示详细说明
+% -----------------------------------------------------------------------------
+%  Torque Clustering - Matlab Implementation
+%  Copyright (C) Jie Yang
+%
+%  Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+%  International (CC BY-NC-SA 4.0)
+%
+%  This code is intended for academic and research purposes only.
+%  Commercial use is strictly prohibited. Please contact the author for licensing inquiries.
+%
+%  Author: Jie Yang (jie.yang.uts@gmail.com)
+% -----------------------------------------------------------------------------
+
+%EVALUATECLUSTER 姝ゅ鏄剧ず鏈夊叧姝ゅ嚱鏁扮殑鎽樿
+%   姝ゅ鏄剧ず璇︾粏璇存槑
 NMI=nmi(Idx,datalabels);
 ACC=accuracy(datalabels,Idx)/100;
 end
