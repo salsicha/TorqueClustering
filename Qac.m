9700ebb05a02436e8ae180e6dd7339410db568a9
Jie Yang <jie.yang.uts@gmail.com>
2025-02-17 14:47:31 +1100

    Update Qac.m

--- Qac.m
+++ Qac.m
@@ -1,6 +1,19 @@
 function [ind] = Qac(sort_p)
-%IND_DEC_1 ´Ë´¦ÏÔÊ¾ÓĞ¹Ø´Ëº¯ÊıµÄÕªÒª ¡£¡£¡£¡£¡£¡£ÏàÁÚÁ½ÊıÏà³ı
-%   ´Ë´¦ÏÔÊ¾ÏêÏ¸ËµÃ÷
+% -----------------------------------------------------------------------------
+%  Torque Clustering - Matlab Implementation
+%  Copyright (C) Jie Yang
+%
+%  Licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+%  International (CC BY-NC-SA 4.0)
+%
+%  This code is intended for academic and research purposes only.
+%  Commercial use is strictly prohibited. Please contact the author for licensing inquiries.
+%
+%  Author: Jie Yang (jie.yang.uts@gmail.com)
+% -----------------------------------------------------------------------------
+
+%IND_DEC_1 æ­¤å¤„æ˜¾ç¤ºæœ‰å…³æ­¤å‡½æ•°çš„æ‘˜è¦ ã€‚ã€‚ã€‚ã€‚ã€‚ã€‚ç›¸é‚»ä¸¤æ•°ç›¸é™¤
+%   æ­¤å¤„æ˜¾ç¤ºè¯¦ç»†è¯´æ˜
 p_num=numel(sort_p);
 ind_num=p_num-1;
 ind=zeros(1,ind_num);
